407796203739d7aa0d4ba03993f9349ae64e0457
nipil <nipil@users.noreply.github.com>
2015-12-11 22:05:10 +0100

    actuel import

--- import.sqlite3.py
+++ import.sqlite3.py
@@ -3,6 +3,7 @@
 import sys
 import json
 import glob
+import time
 import codecs
 import sqlite3
 import os.path
@@ -43,25 +44,46 @@ def open_database(contract_name, station_number):
 	# open or create db file
 	db_path = "~/.jcd/databases/%s_%s.sqlite3" % (contract_name, station_number)
 	full_db_path = os.path.expanduser(db_path)
-	try:
-		return sqlite3.connect(full_db_path)
-	except sqlite3.OperationalError as e:
-		logging.error(e)
-		raise JcdImportException("Failed to open database %s" % full_db_path)
+	connection = sqlite3.connect(full_db_path)
+	return connection
+
+# create table if necessary
+def create_storage_table(connection):
+	connection.execute(
+		'''CREATE TABLE IF NOT EXISTS samples (
+		timestamp INTEGER PRIMARY KEY,
+		bike INTEGER,
+		empty INTEGER,
+		status INTEGER)''')
+
+def import_station_dynamic_data(connection,station):
+	connection.execute(
+		'''INSERT INTO
+		samples (timestamp,bike,empty,status)
+		VALUES(?,?,?,?)''',
+		(station["update"],
+		station["bikes"],
+		station["empty"],
+		station["status"]))
 
 # import contract into database
 def import_contract_station(contract_name, station_number, station):
-	# open/create/init database
-	db = open_database(contract_name, station_number)
-	# create table if necessary
-	pass
-	# import updated stations
-	print "contract %s number %i station %s" % (contract_name, station_number, station)
-	# commit changes
-	pass
+	try:
+		# open or create db file
+		connection = open_database(contract_name, station_number)
+		# due to context, commit on success, auto-rollback on except
+		with connection:
+			# create table if necessary
+			create_storage_table(connection)
+			# import updated stations
+			import_station_dynamic_data(connection, station)
+	except (sqlite3.OperationalError,sqlite3.IntegrityError,sqlite3.Error) as e:
+		logging.error(e)
+		raise JcdImportException("Failed to import station %s" % station)
 
 # import update file into databases
 def import_update(filename):
+	n = 0
 	# loading text content
 	content = load_unicode_file(filename)
 	# load json from text
@@ -73,17 +95,24 @@ def import_update(filename):
 			station = contract[station_key]
 			station_number = int(station_key)
 			import_contract_station(contract_name,station_number,station)
+			n += 1
+	return n
 
 # work
 def work():
 	files = list_files("~/.jcd/updates", "json")
 	for f in files:
 		try:
-			import_update(f)
+			t0 = time.time()
+			n = import_update(f)
+			dt = time.time() - t0
+			rate = None
+			if dt != 0:
+				rate = n / dt
+			logging.info("File %s imported (%s samples in %s seconds, rate %s)" % (f,n,dt,rate))
 		except JcdImportException as e:
 			logging.error(e)
 			logging.warning("Skipping file %s" % f)
-		return
 
 # main
 def main():
