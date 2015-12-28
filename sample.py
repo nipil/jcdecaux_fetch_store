28782f3ae8668b90e55e145418a8c64ee430b71f
nipil <nipil@users.noreply.github.com>
2015-12-28 10:49:02 +0100

    add sample data for offline dev

---
+++ sample.py
@@ -0,0 +1,564 @@
+contracts = [
+    {
+        "cities": [
+            "Rouen"
+        ],
+        "commercial_name": "cy'clic",
+        "country_code": "FR",
+        "name": "Rouen"
+    },
+    {
+        "cities": [
+            "Arcueil",
+            "Aubervilliers",
+            "Bagnolet",
+            "Boulogne Billancourt",
+            "Charenton",
+            "Clichy",
+            "Fontenay-sous-Bois",
+            "Gentilly",
+            "Issy les Moulineaux",
+            "Ivry",
+            "Joinville",
+            "Le Kremlin Bic\u00eatre",
+            "Le Pr\u00e9 St Gervais",
+            "Les Lilas",
+            "Levallois-Perret",
+            "Malakoff",
+            "Montreuil",
+            "Montrouge",
+            "Neuilly",
+            "Nogent",
+            "Pantin",
+            "Paris",
+            "Puteaux",
+            "Saint Cloud",
+            "Saint Denis",
+            "Saint Mand\u00e9",
+            "Saint Maurice",
+            "Saint Ouen",
+            "Suresnes",
+            "Vanves",
+            "Vincennes"
+        ],
+        "commercial_name": "Velib",
+        "country_code": "FR",
+        "name": "Paris"
+    },
+    {
+        "cities": [
+            "Toulouse"
+        ],
+        "commercial_name": "V\u00e9l\u00f4",
+        "country_code": "FR",
+        "name": "Toulouse"
+    },
+    {
+        "cities": [
+            "Luxembourg"
+        ],
+        "commercial_name": "Veloh",
+        "country_code": "LU",
+        "name": "Luxembourg"
+    },
+    {
+        "cities": [
+            "Dublin"
+        ],
+        "commercial_name": "dublinbikes",
+        "country_code": "IE",
+        "name": "Dublin"
+    },
+    {
+        "cities": [
+            "Valencia"
+        ],
+        "commercial_name": "Valenbisi",
+        "country_code": "ES",
+        "name": "Valence"
+    },
+    {
+        "cities": [
+            "Stockholm"
+        ],
+        "commercial_name": "Cyclocity",
+        "country_code": "SE",
+        "name": "Stockholm"
+    },
+    {
+        "cities": [
+            "G\u00f6teborg"
+        ],
+        "commercial_name": "G\u00f6teborg",
+        "country_code": "SE",
+        "name": "Goteborg"
+    },
+    {
+        "cities": [
+            "Santander"
+        ],
+        "commercial_name": "Tusbic",
+        "country_code": "ES",
+        "name": "Santander"
+    },
+    {
+        "cities": [
+            "Lund"
+        ],
+        "commercial_name": "Lundahoj",
+        "country_code": "SE",
+        "name": "Lund"
+    },
+    {
+        "cities": [
+            "Amiens"
+        ],
+        "commercial_name": "Velam",
+        "country_code": "FR",
+        "name": "Amiens"
+    },
+    {
+        "cities": [
+            "Lillestrom"
+        ],
+        "commercial_name": "Bysykkel",
+        "country_code": "NO",
+        "name": "Lillestrom"
+    },
+    {
+        "cities": [
+            "Mulhouse"
+        ],
+        "commercial_name": "V\u00e9loCit\u00e9",
+        "country_code": "FR",
+        "name": "Mulhouse"
+    },
+    {
+        "cities": [
+            "Caluire-et-Cuire",
+            "Lyon",
+            "Vaulx-En-Velin",
+            "Villeurbanne"
+        ],
+        "commercial_name": "V\u00e9lo'V",
+        "country_code": "FR",
+        "name": "Lyon"
+    },
+    {
+        "cities": [
+            "Ljubljana"
+        ],
+        "commercial_name": "Bicikelj ",
+        "country_code": "SI",
+        "name": "Ljubljana"
+    },
+    {
+        "cities": [
+            "Sevillia"
+        ],
+        "commercial_name": "SEVICI",
+        "country_code": "ES",
+        "name": "Seville"
+    },
+    {
+        "cities": [
+            "Namur"
+        ],
+        "commercial_name": "Li bia velo",
+        "country_code": "BE",
+        "name": "Namur"
+    },
+    {
+        "cities": [
+            "Nancy"
+        ],
+        "commercial_name": "v\u00e9lOstan'lib",
+        "country_code": "FR",
+        "name": "Nancy"
+    },
+    {
+        "cities": [
+            "Cr\u00e9teil"
+        ],
+        "commercial_name": "Cristolib",
+        "country_code": "FR",
+        "name": "Creteil"
+    },
+    {
+        "cities": [
+            "Anderlecht",
+            "Berchem-Sainte-Agathe",
+            "Bruxelles",
+            "Etterbeek",
+            "Forest",
+            "Ganshoren",
+            "Ixelles",
+            "Jette",
+            "Koekelberg",
+            "Molenbeek-Saint-Jean",
+            "Saint-Gilles",
+            "Saint-Josse-ten-noode",
+            "Schaerbeek",
+            "Uccle",
+            "Woluwe-Saint-Lambert",
+            "Woluwe-Saint-Pierre"
+        ],
+        "commercial_name": "villo",
+        "country_code": "BE",
+        "name": "Bruxelles-Capitale"
+    },
+    {
+        "cities": [
+            "Cergy",
+            "Courdimanche",
+            "Eragny-sur-Oise",
+            "Neuville-sur-Oise",
+            "Pontoise",
+            "Saint-Ouen-L'Aumone",
+            "Vaur\u00e9al"
+        ],
+        "commercial_name": "Velo2",
+        "country_code": "FR",
+        "name": "Cergy-Pontoise"
+    },
+    {
+        "cities": [
+            "Vilnius"
+        ],
+        "commercial_name": "Cyclocity",
+        "country_code": "LT",
+        "name": "Vilnius"
+    },
+    {
+        "cities": [
+            "Toyama"
+        ],
+        "commercial_name": "cyclocity",
+        "country_code": "JP",
+        "name": "Toyama"
+    },
+    {
+        "cities": [
+            "Kazan"
+        ],
+        "commercial_name": "Veli'k",
+        "country_code": "RU",
+        "name": "Kazan"
+    },
+    {
+        "cities": [
+            "Marseille"
+        ],
+        "commercial_name": "Le v\u00e9lo",
+        "country_code": "FR",
+        "name": "Marseille"
+    },
+    {
+        "cities": [
+            "Nantes"
+        ],
+        "commercial_name": "Bicloo",
+        "country_code": "FR",
+        "name": "Nantes"
+    },
+    {
+        "cities": [
+            "Besan\u00e7on"
+        ],
+        "commercial_name": "V\u00e9loCit\u00e9",
+        "country_code": "FR",
+        "name": "Besancon"
+    }
+]
+
+stations = [
+    {
+        "address": "",
+        "available_bike_stands": 8,
+        "available_bikes": 13,
+        "banking": True,
+        "bike_stands": 21,
+        "bonus": False,
+        "contract_name": "Lund",
+        "last_update": 1451295410000,
+        "name": "ALLHELGONAKYRKAN",
+        "number": 12,
+        "position": {
+            "lat": 55.710366,
+            "lng": 13.192236
+        },
+        "status": "OPEN"
+    },
+    {
+        "address": "",
+        "available_bike_stands": 9,
+        "available_bikes": 20,
+        "banking": True,
+        "bike_stands": 30,
+        "bonus": False,
+        "contract_name": "Lund",
+        "last_update": 1451295256000,
+        "name": "UNIVERSITETSSJUKHUSET",
+        "number": 9,
+        "position": {
+            "lat": 55.7128579364781,
+            "lng": 13.1967929005623
+        },
+        "status": "OPEN"
+    },
+    {
+        "address": "",
+        "available_bike_stands": 27,
+        "available_bikes": 3,
+        "banking": True,
+        "bike_stands": 30,
+        "bonus": False,
+        "contract_name": "Lund",
+        "last_update": 1451295505000,
+        "name": "M\u00c5RTENSTORGET",
+        "number": 5,
+        "position": {
+            "lat": 55.7019107380952,
+            "lng": 13.196025788784
+        },
+        "status": "OPEN"
+    },
+    {
+        "address": "",
+        "available_bike_stands": 21,
+        "available_bikes": 4,
+        "banking": True,
+        "bike_stands": 25,
+        "bonus": False,
+        "contract_name": "Lund",
+        "last_update": 1451295774000,
+        "name": "BANTORGET",
+        "number": 2,
+        "position": {
+            "lat": 55.7042412794077,
+            "lng": 13.1876116991043
+        },
+        "status": "OPEN"
+    },
+    {
+        "address": "",
+        "available_bike_stands": 5,
+        "available_bikes": 15,
+        "banking": True,
+        "bike_stands": 21,
+        "bonus": False,
+        "contract_name": "Lund",
+        "last_update": 1451295390000,
+        "name": "LTH",
+        "number": 11,
+        "position": {
+            "lat": 55.712382,
+            "lng": 13.208109
+        },
+        "status": "OPEN"
+    },
+    {
+        "address": "",
+        "available_bike_stands": 15,
+        "available_bikes": 5,
+        "banking": True,
+        "bike_stands": 20,
+        "bonus": False,
+        "contract_name": "Lund",
+        "last_update": 1451295397000,
+        "name": "GALTEN",
+        "number": 6,
+        "position": {
+            "lat": 55.7008134312229,
+            "lng": 13.1956234574318
+        },
+        "status": "OPEN"
+    },
+    {
+        "address": "",
+        "available_bike_stands": 11,
+        "available_bikes": 15,
+        "banking": True,
+        "bike_stands": 26,
+        "bonus": False,
+        "contract_name": "Lund",
+        "last_update": 1451295356000,
+        "name": "STADSPARKEN NORR",
+        "number": 3,
+        "position": {
+            "lat": 55.7013726686025,
+            "lng": 13.1856563687325
+        },
+        "status": "OPEN"
+    },
+    {
+        "address": "",
+        "available_bike_stands": 2,
+        "available_bikes": 20,
+        "banking": True,
+        "bike_stands": 23,
+        "bonus": False,
+        "contract_name": "Lund",
+        "last_update": 1451295526000,
+        "name": "PROFESSORSGATAN",
+        "number": 16,
+        "position": {
+            "lat": 55.709632,
+            "lng": 13.20595
+        },
+        "status": "OPEN"
+    },
+    {
+        "address": "",
+        "available_bike_stands": 12,
+        "available_bikes": 9,
+        "banking": True,
+        "bike_stands": 21,
+        "bonus": False,
+        "contract_name": "Lund",
+        "last_update": 1451295301000,
+        "name": "UNIVERSITETSBIBLIOTEKET",
+        "number": 8,
+        "position": {
+            "lat": 55.7080949824667,
+            "lng": 13.1963047385216
+        },
+        "status": "OPEN"
+    },
+    {
+        "address": "",
+        "available_bike_stands": 2,
+        "available_bikes": 21,
+        "banking": True,
+        "bike_stands": 23,
+        "bonus": False,
+        "contract_name": "Lund",
+        "last_update": 1451295215000,
+        "name": "BMC",
+        "number": 10,
+        "position": {
+            "lat": 55.7134230454044,
+            "lng": 13.2011756300926
+        },
+        "status": "OPEN"
+    },
+    {
+        "address": "",
+        "available_bike_stands": 20,
+        "available_bikes": 1,
+        "banking": True,
+        "bike_stands": 21,
+        "bonus": False,
+        "contract_name": "Lund",
+        "last_update": 1451295381000,
+        "name": "STORTORGET",
+        "number": 4,
+        "position": {
+            "lat": 55.7029777865809,
+            "lng": 13.1933650374413
+        },
+        "status": "OPEN"
+    },
+    {
+        "address": "",
+        "available_bike_stands": 11,
+        "available_bikes": 8,
+        "banking": True,
+        "bike_stands": 20,
+        "bonus": False,
+        "contract_name": "Lund",
+        "last_update": 1451295682000,
+        "name": "BOTANISKA TR\u00c4DG\u00c5RDEN",
+        "number": 7,
+        "position": {
+            "lat": 55.7059732259213,
+            "lng": 13.201240003109
+        },
+        "status": "OPEN"
+    },
+    {
+        "address": "",
+        "available_bike_stands": 4,
+        "available_bikes": 20,
+        "banking": True,
+        "bike_stands": 24,
+        "bonus": False,
+        "contract_name": "Lund",
+        "last_update": 1451295626000,
+        "name": "SPARTA",
+        "number": 15,
+        "position": {
+            "lat": 55.709257,
+            "lng": 13.214571
+        },
+        "status": "OPEN"
+    },
+    {
+        "address": "",
+        "available_bike_stands": 28,
+        "available_bikes": 3,
+        "banking": True,
+        "bike_stands": 31,
+        "bonus": False,
+        "contract_name": "Lund",
+        "last_update": 1451295221000,
+        "name": "LUNDS CENTRAL",
+        "number": 1,
+        "position": {
+            "lat": 55.7065202995297,
+            "lng": 13.1874185800552
+        },
+        "status": "OPEN"
+    },
+    {
+        "address": "",
+        "available_bike_stands": 3,
+        "available_bikes": 18,
+        "banking": True,
+        "bike_stands": 21,
+        "bonus": False,
+        "contract_name": "Lund",
+        "last_update": 1451295603000,
+        "name": "IDEON S\u00d6DER",
+        "number": 14,
+        "position": {
+            "lat": 55.712961,
+            "lng": 13.213704
+        },
+        "status": "OPEN"
+    },
+    {
+        "address": "",
+        "available_bike_stands": 7,
+        "available_bikes": 15,
+        "banking": True,
+        "bike_stands": 23,
+        "bonus": False,
+        "contract_name": "Lund",
+        "last_update": 1451295355000,
+        "name": "FYSIOLOGEN",
+        "number": 17,
+        "position": {
+            "lat": 55.710874,
+            "lng": 13.202782
+        },
+        "status": "OPEN"
+    },
+    {
+        "address": "",
+        "available_bike_stands": 3,
+        "available_bikes": 21,
+        "banking": True,
+        "bike_stands": 24,
+        "bonus": False,
+        "contract_name": "Lund",
+        "last_update": 1451295505000,
+        "name": "IDEON NORR",
+        "number": 13,
+        "position": {
+            "lat": 55.715504,
+            "lng": 13.214801
+        },
+        "status": "OPEN"
+    }
+]
